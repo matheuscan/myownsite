588a1284ea0b2e03db372b3d6362bc8ec4dae4cd
Germán Matheus <111332528+matheuscan@users.noreply.github.com>
2024-06-28 02:34:59 -0400

    consts updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'matheuscan';
+export const SITE_DESCRIPTION = 'Sitio web del usuario Germán Matheus';
