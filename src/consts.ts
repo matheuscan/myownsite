daace99a282117a60f7e633f15253c08dae5182d
Germán Matheus <111332528+matheuscan@users.noreply.github.com>
2024-07-22 03:33:38 -0400

    Delete src directory

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'matheuscan';
-export const SITE_DESCRIPTION = 'Sitio web del usuario Germán Matheus';
