8ef88ec60321303d94bd6f748a5c6ba359ab28b6
Germán Matheus <germanmatheus.dev@gmail.com>
2024-07-24 03:54:35 -0430

    Build issues solved

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Nav from './components/Nav'
 import Home from './components/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Blog from './components/Blog'
-import Footer from './components/Footer'
+import Footer from './components/Footer.tsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
