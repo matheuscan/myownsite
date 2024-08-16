961a6850a117b1579747d97707826dbb5c76831e
German Matheus <germanmatheus.dev@gmail.com>
2024-08-16 04:09:31 -0400

    responsive navbar added, background changed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,34 @@
 import '../assets/css/nav.css'
+import { useState } from 'react'
+
+
 
 const Nav = () => {
+    const [state,setState] =useState("offcanvas")
+    let [mainNavState, setMainNav] = useState("")
+    let [srcState, setSrc] = useState("../../public/img/hamburger-menu.png")
+
+    const handleClick = () => {
+        if (state === "offcanvas") 
+        {
+            setState("offcanvas-clicked")
+            setMainNav("disappear")
+            setSrc("../../public/img/close.png")
+        }
+        else
+           {
+            setState("offcanvas")
+            setMainNav("")
+            setSrc("../../public/img/hamburger-menu.png")
+           }
+        
+    }
     return (
         <>
             <nav>
-                <a href="/" id="brand">Germán Matheus</a>
+                <a href="/" id="brand" className={mainNavState}>Germán Matheus</a>
 
-                <ul>
+                <ul className={mainNavState}>
                     <a href="/" className="nav-item">Home</a>
                     <a href="/blog" className="nav-item">Blog</a>
                     <a href="" className="nav-item"></a>
@@ -16,7 +38,23 @@ const Nav = () => {
                     <a href="https://linkedin.com/in/german-matheus"><img src=".../../public/img/linkedin-icon.png" alt="Linkedin" className="nav-icon" style={{'marginRight':'1rem'}}/></a>
                     <a href="https://github.com/matheuscan"><img src=".../../public/img/github.png" alt="GitHub" className="nav-icon"/></a>
                 </div>
-            </nav>
+
+                <button id='btnNav' onClick={handleClick}><img src={srcState} alt="" /></button>
+                <div className={state}>
+                   <div className="links-container">
+                    
+                    <a href="#">Home</a>
+                    <a href="#">Home</a>
+                    <a href="#">Home</a>
+                    <div>
+                        <a href="https://linkedin.com/in/german-matheus"><img src=".../../public/img/linkedin-icon.png" alt="Linkedin" className="nav-icon" style={{'marginRight':'1rem'}}/></a>
+                        <a href="https://github.com/matheuscan"><img src=".../../public/img/github.png" alt="GitHub" className="nav-icon"/></a>
+                     </div>
+                   </div>
+                </div>
+            
+
+                </nav>
         </>
     )
 }
